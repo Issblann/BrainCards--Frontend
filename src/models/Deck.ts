82960a9014215056c69fec148843c1800aa0f226
Issblann <anaisabelpp@outlook.com>
2024-09-18 13:10:03 -0500

    add styles flip card and handler cards with values and get decks with flashcards

--- src/models/Deck.ts
+++ src/models/Deck.ts
@@ -1,8 +1,11 @@
+import Flashcard from './Flashcards';
+
 interface Deck {
   id: string | number;
   boxId?: string;
   title: string;
   description?: string;
+  flashCards?: Flashcard[];
 }
 
 export default Deck;
