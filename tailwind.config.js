8df37dfe81f3606fed026461620c3ac44a25f1b8
issblann <anaisabelpp@outlook.com>
2025-04-27 15:58:54 -0500

    Feature: Agregar animaciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,15 @@ module.exports = withMT({
           950: '#441f4c',
         },
       },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: 0, transform: 'scale(0.95)' },
+          '100%': { opacity: 1, transform: 'scale(1)' },
+        }
+      },
+      animation: {
+        'fade-in': 'fade-in 0.3s ease-out'
+      },
     },
 
     fontFamily: {
